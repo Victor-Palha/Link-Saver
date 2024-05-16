5616019611cdb98b26013abd2fcb83aa4f14c181
strahd <jv.palha@gmail.com>
2024-05-16 01:49:02 -0300

    change build file

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@
 mix deps.get --only prod
 MIX_ENV=prod mix compile
 MIX_ENV=prod mix assets.deploy
-MIX_ENV=prod mix release
 MIX_ENV=prod mix ecto.migrate
-_build/prod/rel/demo/bin/demo start
+MIX_ENV=prod mix release
